9db419794bed5fdc13b35db2002f22fa5419908b
Ëwerton Dias <ton.14@hotmail.com>
2020-07-29 16:26:15 -0300

    change in the script

--- scripts/script290720.js
+++ scripts/script290720.js
@@ -1,1 +1,19 @@
-console.log("pronto");
+
+function isPairOdd(number){
+    return number %2 == 0 ? "É par" : "É ímpar";
+}
+console.log(isPairOdd(13));
+
+let isCousin = (num) => {
+    for (var i = 2; i < num; i++)
+    if (num % i === 0) return "Nao é primo";
+    return  (num !== 1) ? "É primo" : "Nao é primo" ;
+}
+console.log(isCousin(4));
+
+function regressiveTime(number){
+    
+    setTimeout(function(){ alert("Hello"); }, 3000);
+}
+
+regressiveTime(10);
